6a93882dfdf9bfd5f21d899753c65c20bd1dcfc7
BunnySasi <sasikiran2213@gmail.com>
2025-07-14 10:53:33 +0530

    commit

---
+++ TechWithJatin/src/main/java/TechWithJatin/TechWithJatin/App.java
@@ -0,0 +1,10 @@
+package TechWithJatin.TechWithJatin;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
